66956c56cf2facb925d23bf7bc5f3e339daf4033
fgmcolas <https://github.com/fgmcolas>
2024-04-22 16:33:49 +0200

    testing without generator import

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,9 @@
 """ a coroutine called async_generator that takes no arguments """
 import random
 import asyncio
-from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator():
     """ coroutine that yields random numbers asynchronously """
     for num in range(10):
         await asyncio.sleep(1)
