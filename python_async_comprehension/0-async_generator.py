40457235aa5b6c82a0cb5b277584cf1c63a9d5c6
fgmcolas <https://github.com/fgmcolas>
2024-04-22 16:36:32 +0200

    testing with incompatible generator method

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ a coroutine called async_generator that takes no arguments """
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ coroutine that yields random numbers asynchronously """
     for num in range(10):
         await asyncio.sleep(1)
