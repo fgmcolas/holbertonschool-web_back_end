c350bfe0bbf8116b134d193c2dca11af3f267de7
fgmcolas <https://github.com/fgmcolas>
2024-04-22 16:31:06 +0200

    adding asyncgenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ a coroutine called async_generator that takes no arguments """
 import random
 import asyncio
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """ coroutine that yields random numbers asynchronously """
     for num in range(10):
         await asyncio.sleep(1)
