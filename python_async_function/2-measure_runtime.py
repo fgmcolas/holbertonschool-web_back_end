fc747e2553566df8161f88a3ce8fc9c88ea7fd52
fgmcolas <https://github.com/fgmcolas>
2024-04-22 15:43:01 +0200

    correcting whitespace for pycodestyle

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,7 +3,6 @@
 wait_n(n, max_delay) and return total_time / n """
 import asyncio
 import time
-
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
@@ -11,7 +10,7 @@ def measure_time(n: int, max_delay: int) -> float:
     """ measure total execution time for
     wait_n(n, max_delay) and return total_time / n """
     start_time = time.perf_counter()
-    asyncio.run (wait_n(n, max_delay))
+    asyncio.run(wait_n(n, max_delay))
     end_time = time.perf_counter()
     total_time = end_time - start_time
     return total_time / n
