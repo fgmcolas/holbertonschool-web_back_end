7c8347a55a86ae8077c890d0a849edc55b3778c0
fgmcolas <https://github.com/fgmcolas>
2024-04-22 15:02:40 +0200

    adding all exercice working locally with pycodestyle verified + README.md

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" measure the total execution time for
+wait_n(n, max_delay) and return total_time / n"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """measure total execution time for
+    wait_n(n, max_delay) and return total_time / n."""
+    start_time = time.time()
+    await wait_n(n, max_delay)
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
