b755200f7e4a75d269e01fe0621d5b47c9928192
fgmcolas <https://github.com/fgmcolas>
2024-04-22 15:41:22 +0200

    correcting methods

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 """ measure the total execution time for
-wait_n(n, max_delay) and return total_time / n"""
+wait_n(n, max_delay) and return total_time / n """
 import asyncio
 import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
-    """measure total execution time for
-    wait_n(n, max_delay) and return total_time / n."""
-    start_time = time.time()
-    await wait_n(n, max_delay)
-    end_time = time.time()
+def measure_time(n: int, max_delay: int) -> float:
+    """ measure total execution time for
+    wait_n(n, max_delay) and return total_time / n """
+    start_time = time.perf_counter()
+    asyncio.run (wait_n(n, max_delay))
+    end_time = time.perf_counter()
     total_time = end_time - start_time
     return total_time / n
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,5 +7,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ create asyncio.Tasks for wait_random """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    tasks = []
+    for _ in range(n):
+        tasks.append(task_wait_random(max_delay))
+    sorted_delays = await asyncio.gather(*tasks)
+    return sorted(sorted_delays)
