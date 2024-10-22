6fdedd9b833f4a56e57c948a9d5a0da4171f831f
fgmcolas <https://github.com/fgmcolas>
2024-10-22 04:42:09 +0200

    i18n

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
-"""Base flask app"""
-from flask import Flask, render_template, request
+""" Route module for the API - Get locale from request"""
+from flask import Flask, request, render_template
 from flask_babel import Babel
+from os import getenv
+
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
 class Config(object):
-    """Setups"""
-    LANGUAGES = ["en", "fr"]
+    """ Setup - Babel configuration """
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
@@ -17,18 +19,21 @@ class Config(object):
 app.config.from_object(Config)
 
 
-@app.route('/')
-def home():
-    """ Home Page
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ GET /
+    Return: 2-index.html
     """
     return render_template('2-index.html')
 
 
 @babel.localeselector
-def get_locale():
-    """Get locale"""
+def get_locale() -> str:
+    """ Determines best match for supported languages """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", "5000")
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
