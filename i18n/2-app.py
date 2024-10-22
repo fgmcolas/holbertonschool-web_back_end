f1e4fcea3adb741d88305bc892c90a7341b45420
fgmcolas <https://github.com/fgmcolas>
2024-10-22 15:39:21 +0200

    Task 2

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Route module for the API - Get locale from request"""
+
 from flask import Flask, request, render_template
 from flask_babel import Babel
-from os import getenv
 
 
 app = Flask(__name__)
@@ -19,21 +19,20 @@ class Config(object):
 app.config.from_object(Config)
 
 
+
+@babel.locale_selector
+def get_locale():
+    """ Determines best match for supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> str:
+def index():
     """ GET /
     Return: 2-index.html
     """
     return render_template('2-index.html')
 
 
-@babel.locale_selector
-def get_locale() -> str:
-    """ Determines best match for supported languages """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
 if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run()
