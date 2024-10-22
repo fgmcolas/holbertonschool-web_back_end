e19585991d32a2d0eb7c42a75c76854e02a59a3d
fgmcolas <https://github.com/fgmcolas>
2024-10-22 18:29:41 +0200

    Task 3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -28,7 +28,7 @@ def index() -> str:
     return render_template('3-index.html')
 
 
-@babel.localeselector
+@babel.locales_elector
 def get_locale() -> str:
     """ Determines best match for supported languages """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
