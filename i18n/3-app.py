1c392fa12a9769cc5d578132c0c42b71cccd90ba
fgmcolas <https://github.com/fgmcolas>
2024-10-23 14:56:39 +0200

    Task 3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -29,9 +29,9 @@ babel = Babel(app, locale_selector=get_locale)
 @app.route('/')
 def index():
     """ GET /
-    Return: 2-index.html
+    Return: 3-index.html
     """
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
