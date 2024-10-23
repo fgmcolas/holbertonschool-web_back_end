9f16e8add64ee5377fd0b74b744fc23ca67e9b53
fgmcolas <https://github.com/fgmcolas>
2024-10-23 14:25:42 +0200

    Task 4

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,43 +1,40 @@
 #!/usr/bin/env python3
-""" Route module for the API - Force locale with URL parameter """
+""" Route module for the API - Get locale from request"""
 from flask import Flask, request, render_template
 from flask_babel import Babel
-from os import getenv
+
 
 app = Flask(__name__)
-babel = Babel(app)
 
 
-class Config(object):
+class Config:
     """ Setup - Babel configuration """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app.config.from_object('4-app.Config')
+app.config.from_object(Config)
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> str:
-    """ GET /
-    Return: 4-index.html
-    """
-    return render_template('4-index.html')
-
-
-@babel.localeselector
-def get_locale() -> str:
+def get_locale():
     """ Determines best match for supported languages """
-    if request.args.get('locale'):
-        locale = request.args.get('locale')
-        if locale in app.config['LANGUAGES']:
-            return locale
-    else:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Babel(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    """ GET /
+    Return: 2-index.html
+    """
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run()
