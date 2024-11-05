d99c6df290dec23b83a10ce2e668c7c4150d9069
fgmcolas <https://github.com/fgmcolas>
2024-11-05 01:21:16 +0100

    Updating indentation

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Task: 0. We are all unique! - creates a table `users` with a unique constraint on the `id` column
 -- Script can be executed on any database
-CREATE TABLE If NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `users` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL UNIQUE,
     `name` VARCHAR(255)
