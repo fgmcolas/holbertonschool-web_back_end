5760235bdcbe28af9a6fe8d4165f7b347680b94a
fgmcolas <https://github.com/fgmcolas>
2024-11-05 01:26:15 +0100

    Updating indentation

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Task: 3. Old school bandlists - all bands with Glam as their main style
 -- Script can be executed on any database
 SELECT DISTINCT `band_name`,
-    IFNULL(`split`, 2020) - `formed` as `lifespan`
+    IFNULL(`split`, 2020) - `formed` AS `lifespan`
 FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
 ORDER BY `lifespan` DESC;
