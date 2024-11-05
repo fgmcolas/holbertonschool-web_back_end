14c847854decf06354828151adc402e3e2c8a382
fgmcolas <https://github.com/fgmcolas>
2024-11-05 01:14:55 +0100

    Changing indentation

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Task: 3. Old school band - inserts rows into the `users` table with the `age` column set to a value under 21
 -- Script can be executed on any database
 SELECT DISTINCT band_name,
-IFNULL(split, 2020) - formed AS lifespan
-FROM metal_bands
+    IFNULL('split', 2020) - 'formed' AS 'lifespan'
+FROM 'metal_bands'
 WHERE FIND_IN_SET('Glam rock', style)
-ORDER BY lifespan DESC;
+ORDER BY 'lifespan' DESC;
