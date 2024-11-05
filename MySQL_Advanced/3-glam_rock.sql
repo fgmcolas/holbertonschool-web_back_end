29b7ccb2fdd62d222f50ca3084c0de8b23580b14
fgmcolas <https://github.com/fgmcolas>
2024-11-05 01:32:23 +0100

    New conditions for checkers

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,6 @@
--- Task: 3. Old school bandlists - all bands with Glam as their main style
--- Script can be executed on any database
-SELECT name AS band_name,
-    IFNULL(YEAR(split) - YEAR(formed), 0) AS lifespan
-FROM metal_bands
-WHERE style = 'Glam rock'
-ORDER BY lifespan DESC;
+-- Task 3: Old school bandlists - all bands with Glam as their main style
+-- ranked by their longevity
+SELECT DISTINCT `band_name`,
+                IFNULL(`split`, 2020) - `formed` as `lifespan`
+    FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
+    ORDER BY `lifespan` DESC;
