4b3cf300eb59edef211a07bda0871929baff4fd7
fgmcolas <https://github.com/fgmcolas>
2024-11-05 01:19:36 +0100

    Updating indentation

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
--- Task: 0. We are all unique! - creates a table `users` with a unique constraint on the `id` column to ensure no duplicate entries
+-- Task: 0. We are all unique! - creates a table `users` with a unique constraint on the `id` column
 -- Script can be executed on any database
-CREATE TABLE IF NOT EXISTS 'users' (
-    'id' INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    'email' VARCHAR(255) NOT NULL UNIQUE,
-    'name' VARCHAR(255)
+CREATE TABLE If NOT EXISTS `users` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255)
 );
--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
--- Task: 2. Best band ever! - inserts a row into the `users` table with specific values for `name`, `email`, and `age`
+-- Task: 2. Best band ever! - inserts a row into the `users` table
 -- Script can be executed on any database
-SELECT DISTINCT 'origin', SUM('fans') AS 'nb_fans' FROM 'metal_bands'
-GROUP BY 'origin'
-ORDER BY 'nb_fans' DESC;
+SELECT DISTINCT `origin`, SUM(`fans`) as `nb_fans` FROM `metal_bands`
+GROUP BY `origin`
+ORDER BY `nb_fans` DESC;
