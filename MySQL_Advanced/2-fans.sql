34da45eed1b9ab0d1952aaeb2bcce45dbc7ac6fa
fgmcolas <https://github.com/fgmcolas>
2024-11-05 01:17:29 +0100

    Updating indentation

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,6 +1,5 @@
 -- Task: 2. Best band ever! - inserts a row into the `users` table with specific values for `name`, `email`, and `age`
 -- Script can be executed on any database
-SELECT DISTINCT 'origin', SUM('fans') AS 'nb_fans'
-FROM 'metal_bands'
+SELECT DISTINCT 'origin', SUM('fans') AS 'nb_fans' FROM 'metal_bands'
 GROUP BY 'origin'
 ORDER BY 'nb_fans' DESC;
