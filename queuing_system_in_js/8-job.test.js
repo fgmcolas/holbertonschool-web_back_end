468632927a2e0b8adfb60feee9cd3479e88bfaa0
fgmcolas <https://github.com/fgmcolas>
2024-11-13 06:15:32 +0100

    Task 11

--- queuing_system_in_js/8-job.test.js
+++ queuing_system_in_js/8-job.test.js
@@ -28,27 +28,23 @@ describe('createPushNotificationsJobs', () => {
     expect(() => {
       createPushNotificationsJobs(2, queue);
     }).to.throw('Jobs is not an array');
-    // expect(queue.testMode.job[0]);
   });
 
   it('display a error message if jobs is not an array passing Object', () => {
     expect(() => {
       createPushNotificationsJobs({}, queue);
     }).to.throw('Jobs is not an array');
-    // expect(queue.testMode.job[0]);
   });
 
   it('display a error message if jobs is not an array passing String', () => {
     expect(() => {
       createPushNotificationsJobs('Hello', queue);
     }).to.throw('Jobs is not an array');
-    // expect(queue.testMode.job[0]);
   });
 
   it('should NOT display a error message if jobs is an array with empty array', () => {
     const ret = createPushNotificationsJobs([], queue);
     expect(ret).to.equal(undefined);
-    // expect(queue.testMode.job[0]);
   });
 
   it('create two new jobs to the queue', () => {
@@ -61,4 +57,3 @@ describe('createPushNotificationsJobs', () => {
     expect(queue.testMode.jobs[1].data).to.eql({ baz: 'bip' });
   });
 });
-
